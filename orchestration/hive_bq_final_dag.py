6fdcab090aecf348579af0b31d5e1dfbfaeecd52
bhattashi <bhattashi05@gmail.com>
2026-03-11 12:54:01 +0530

    Update hive_bq_final_dag.py

--- orchestration/hive_bq_final_dag.py
+++ orchestration/hive_bq_final_dag.py
@@ -5,8 +5,10 @@ from airflow import DAG
 from airflow.utils.task_group import TaskGroup
 from airflow.providers.google.cloud.operators.cloud_storage_transfer_service import CloudDataTransferServiceRunJobOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
-from airflow.providers.apache.hdfs.sensors.web_hdfs import WebHdfsSensor
-from airflow.providers.http.sensors.http import HttpSensor
+# from airflow.providers.apache.hdfs.sensors.web_hdfs import WebHdfsSensor
+# from airflow.providers.http.sensors.http import HttpSensor
+import requests
+from airflow.operators.python import PythonOperator
 # from airflow.providers.apache.hdfs.sensors.hdfs import HdfsSensor
 
 # --- CONFIGURATION ---
@@ -24,6 +26,22 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+# DEFINE YOUR FUNCTIONS HERE (Above the DAG) ---
+def check_hdfs_file():
+    # Use the current IP of your master node
+    url = "http://10.128.0.22:9870/webhdfs/v1/user/data/demo_migration/loans_ac/_SUCCESS?op=GETFILESTATUS"
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+            print("Successfully found _SUCCESS file.")
+            return True
+        else:
+            # This triggers a retry if the file isn't there yet
+            raise Exception(f"File not found yet. Status: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        # This triggers a retry if the network times out
+        raise Exception(f"Network error: {e}")
+
 with DAG(
     "hdfs_to_bq_production_pipeline",
     default_args=default_args,
@@ -39,16 +57,13 @@ with DAG(
         with TaskGroup(group_id=f"process_{table}") as table_group:
             
             # 1. SENSOR: Wait for Dataproc HDFS flag
-            wait_for_hdfs_success = HttpSensor(
+            wait_for_hdfs_success = PythonOperator(
                 task_id="wait_for_hdfs_success",
-                http_conn_id="webhdfs_http", # Create a standard 'HTTP' type connection
+                python_callable=check_hdfs_file,
                 endpoint=f"/webhdfs/v1/user/data/demo_migration/{table}/_SUCCESS?op=GETFILESTATUS",
                 ## hdfs_conn_id="on_prem_cdp_hdfs", # Points to the new WebHDFS connection # Airflow UI (Admin > Connections)
-                method="GET",
-                response_check=lambda response: response.status_code == 200,
-                poke_interval=60,
-                timeout=3600, # 1 hour
-                extra_options={"timeout": 30}, # Wait 30s for the master to respond to the ping
+                retries=10,
+                retry_delay=timedelta(minutes=1),
                 mode="reschedule"
             )
 
